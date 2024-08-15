094a8cfc8a52f51a02a074dd8832acef2911f808
owen alterolab <owen@alterolab.com>
2024-08-15 15:31:13 +0800

    chapter5-sub01

--- chapter4/model07.py
+++ chapter4/model07.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 from langchain_teddynote import logging
-from langchain_huggingface import HuggingFacePipeline
+from langchain_huggingface import HuggingFacePipeline, HuggingFaceEndpoint
 import os
 from langchain_core.prompts import PromptTemplate
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 load_dotenv()
 logging.langsmith("CH04-Models")
@@ -11,33 +12,28 @@ os.environ["TRANSFORMERS_CACHE"] = "./cache/"
 os.environ["HF_HOME"] = "./cache/"
 
 llm = HuggingFacePipeline.from_model_id(
-    model_id="microsoft/Phi-3-mini-4k-instruct",
+    model_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
     task="text-generation",
-    pipeline_kwargs={
-        "max_new_tokens": 256,
-        "top_k": 50,
-        "temperature": 0.1,
-        "do_sample": True,
-    },
+    pipeline_kwargs={"max_new_tokens": 10},
 )
 print(llm.invoke("Hugging Face is"))
 
-template = """Summarizes TEXT in simple bullet points ordered from most important to least important.
-TEXT:
-{text}
+# template = """Summarizes TEXT in simple bullet points ordered from most important to least important.
+# TEXT:
+# {text}
 
-KeyPoints: """
+# KeyPoints: """
 
-# 프롬프트 템플릿 생성
-prompt = PromptTemplate.from_template(template)
+# # 프롬프트 템플릿 생성
+# prompt = PromptTemplate.from_template(template)
 
-# 체인 생성
-chain = prompt | llm
+# # 체인 생성
+# chain = prompt | llm
 
-text = """A Large Language Model (LLM) like me, ChatGPT, is a type of artificial intelligence (AI) model designed to understand, generate, and interact with human language. These models are "large" because they're built from vast amounts of text data and have billions or even trillions of parameters. Parameters are the aspects of the model that are learned from training data; they are essentially the internal settings that determine how the model interprets and generates language. LLMs work by predicting the next word in a sequence given the words that precede it, which allows them to generate coherent and contextually relevant text based on a given prompt. This capability can be applied in a variety of ways, from answering questions and composing emails to writing essays and even creating computer code. The training process for these models involves exposing them to a diverse array of text sources, such as books, articles, and websites, allowing them to learn language patterns, grammar, facts about the world, and even styles of writing. However, it's important to note that while LLMs can provide information that seems knowledgeable, their responses are generated based on patterns in the data they were trained on and not from a sentient understanding or awareness. The development and deployment of LLMs raise important considerations regarding accuracy, bias, ethical use, and the potential impact on various aspects of society, including employment, privacy, and misinformation. Researchers and developers continue to work on ways to address these challenges while improving the models' capabilities and applications."""
+# text = """A Large Language Model (LLM) like me, ChatGPT, is a type of artificial intelligence (AI) model designed to understand, generate, and interact with human language. These models are "large" because they're built from vast amounts of text data and have billions or even trillions of parameters. Parameters are the aspects of the model that are learned from training data; they are essentially the internal settings that determine how the model interprets and generates language. LLMs work by predicting the next word in a sequence given the words that precede it, which allows them to generate coherent and contextually relevant text based on a given prompt. This capability can be applied in a variety of ways, from answering questions and composing emails to writing essays and even creating computer code. The training process for these models involves exposing them to a diverse array of text sources, such as books, articles, and websites, allowing them to learn language patterns, grammar, facts about the world, and even styles of writing. However, it's important to note that while LLMs can provide information that seems knowledgeable, their responses are generated based on patterns in the data they were trained on and not from a sentient understanding or awareness. The development and deployment of LLMs raise important considerations regarding accuracy, bias, ethical use, and the potential impact on various aspects of society, including employment, privacy, and misinformation. Researchers and developers continue to work on ways to address these challenges while improving the models' capabilities and applications."""
 
-# chain 실행
-response = chain.invoke({"text": text})
+# # chain 실행
+# response = chain.invoke({"text": text})
 
-# 결과 출력
-print(response)
+# # 결과 출력
+# print(response)
---
+++ chapter5/memory01.py
@@ -0,0 +1,60 @@
+from langchain.memory import ConversationBufferMemory
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationChain
+
+load_dotenv()
+# memory = ConversationBufferMemory()
+# Mengatur return_messages = True akan mengembalikan objek HumanMessage dan AIMessage.
+memory = ConversationBufferMemory(return_messages=True)
+
+memory.save_context(
+    inputs = {
+        "human": "Hai, saya ingin membuka rekening bank secara tatap muka, bagaimana cara memulainya?"
+    },
+    outputs = {
+        "ai": "Halo, kami senang mendengar Anda ingin membuka rekening. Pertama, bisakah Anda menyiapkan kartu identitas untuk memverifikasi identitas Anda?"
+    },
+)
+
+memory.save_context(
+    inputs={"human": "Ya, saya memiliki ID saya, sekarang apa yang harus saya lakukan?"},
+    outputs = {
+        "ai": "Terima kasih. Silakan ambil foto yang jelas dari bagian depan dan belakang kartu identitas Anda dan unggah, dan kami akan melanjutkan untuk memverifikasi identitas Anda."
+    },
+)
+
+memory.save_context(
+    inputs = {"manusia": "Saya telah mengunggah foto, bagaimana cara memverifikasi identitas saya?"},
+    outputs = {
+        "ai": "Kami telah memverifikasi foto yang Anda unggah, sekarang silakan verifikasi identitas Anda melalui ponsel Anda. Silakan masukkan nomor verifikasi yang dikirimkan kepada Anda melalui SMS."
+    },
+)
+memory.save_context(
+    inputs = {"manusia": "Saya sudah memasukkan nomor verifikasi, sekarang bagaimana cara membuka akun?" },
+    outputs = {
+        "ai": "Identitas Anda telah diverifikasi, sekarang silakan pilih jenis akun yang Anda inginkan dan isi informasi yang diperlukan. Anda bisa memilih jenis deposit, mata uang, dll."
+    },
+)
+
+# print(memory.load_memory_variables({}))
+#print(memory.load_memory_variables({})["history"])
+
+#conversation chain
+llm = ChatOpenAI(temperature=0)
+
+conversation = ConversationChain(
+    llm=llm,
+    memory=ConversationBufferMemory(),
+)
+
+# Memulai dialog.
+response = conversation.predict(
+    input="Hai, saya ingin membuka rekening bank secara tatap muka, bagaimana cara memulainya?"
+)
+print(response)
+
+response = conversation.predict(
+    input = "Tolong rangkum jawaban Anda sebelumnya ke dalam poin-poin."
+)
+print(response)
