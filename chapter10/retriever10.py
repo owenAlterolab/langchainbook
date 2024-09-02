4698e26ea0b4356c97363e7d26c4abef392f7d44
owenAlterolab <owen@alterolab.com>
2024-09-02 10:04:44 +0800

    continue to retriever10

--- chapter10/retriever10.py
+++ chapter10/retriever10.py
@@ -1,6 +1,9 @@
 from langchain_community.retrievers import BM25Retriever
 from langchain_altero.retrievers import NLTKBM25Retriever
 
+import nltk
+nltk.download('punkt')
+
 def pretty_print(docs):
     for i, doc in enumerate(docs):
         if "score" in doc.metadata:
@@ -15,5 +18,25 @@ sample_texts = [
     "Asuransi keuangan bom besar lebih berfokus pada perlindungan risiko daripada tabungan. Produk ini cocok untuk pelanggan yang bersedia mengambil risiko tinggi.",
 ]
 
+
 nltk = NLTKBM25Retriever.from_texts(sample_texts)
-pretty_print(nltk.invoke("Asuransi Keuangan"))
+pretty_print(nltk.invoke("Asuransi keuangan"))
+
+print()
+
+pretty_print(nltk.search_with_score("Asuransi keuangan"))
+
+print()
+
+bm25 = BM25Retriever.from_texts(sample_texts)
+
+print(f'Nltk:  \t {nltk.invoke("Asuransi keuangan")[0].page_content}')
+print(f'BM25:  \t {bm25.invoke("Asuransi keuangan")[0].page_content}')
+
+print()
+
+# Membuat retriever.
+kiwi = NLTKBM25Retriever.from_texts(sample_texts)
+kiwi.k = 2
+# Melakukan pencarian berdasarkan kemiripan.
+pretty_print(kiwi.search_with_score("Asuransi keuangan"))
